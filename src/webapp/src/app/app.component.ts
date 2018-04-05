0dda0cda92021fb185b56532f79d10d5d7c5ee91
Gvo3d <denis.yakimov89@gmail.com>
2018-04-06 00:37:59 +0300

    init

---
+++ src/webapp/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MessageService} from "./message.service";
+import {Subscription} from "rxjs/Subscription";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'app works!';
+  messages: Array<string> = [];
+
+  messageSub: Subscription;
+
+  constructor(private messageService: MessageService) {
+  }
+
+
+
+  ngOnInit(): void {
+    this.messageSub = this.messageService.messageReceived$.subscribe( message => this.messages.push(message));
+  }
+
+  ngOnDestroy(): void {
+    if ( this.messageSub ) {
+      this.messageSub.unsubscribe();
+    }
+  }
+
+  onClickMe(): void {
+    this.messageService.stompClient.send("/app/hello", {}, "{\"name\":\"fucker\"}" )
+  }
+}
